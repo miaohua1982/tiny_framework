7d7e587fe3627b3afd64d9103e4ba0d80f1db85f
371462426@qq.com <371462426@qq.com>
2021-09-13 18:07:51 +0800

    add conv backward in cpp file

--- framework/conv_operations/src/operations.cpp
+++ framework/conv_operations/src/operations.cpp
@@ -16,7 +16,7 @@ inline int max(int a, int b){
     return a<b? b:a;
 }
 
-float multiply(float * kernel, float * data, dh, dw, kh, kw, input_channels) 
+float multiply(float * kernel, float * data, size_t dh, size_t dw, size_t kh, size_t kw, size_t input_channels) 
 {
     float sum = 0;
     int kernel_inter = kh*kw;
@@ -57,8 +57,6 @@ void conv2d_forward(const py::array_t<float>& feat_input, const py::array_t<floa
     //inns
     size_t dh = feat_input_buf.shape[2];
     size_t dw = feat_input_buf.shape[3];
-    size_t output_width = (dw+padding*2-kw)/stride+1;
-    size_t output_height = (dh+padding*2-kh)/stride+1;
     size_t feat_h = dh;
     size_t feat_w = dw;
 
@@ -76,23 +74,23 @@ void conv2d_forward(const py::array_t<float>& feat_input, const py::array_t<floa
             for(int j = 0; j < input_channels; ++j) {
                 for(int k = 0; k < dh; ++k) {
                     //                           base        padding     done                      base    done
-                    memcpy(new_input_ptr+i*j*feat_img_size+padding_zero+k*feat_w, feat_input_ptr+i*j*dh*dw+k*dw, dw*sizeof(float))
+                    memcpy(new_input_ptr+i*j*feat_img_size+padding_zero+k*feat_w, feat_input_ptr+i*j*dh*dw+k*dw, dw*sizeof(float));
                 }
             }
 
         feat_input_ptr = new_input_ptr;
     }
 
-    auto output = feat_output.unchecked<4>();
+    auto output = feat_output.mutable_unchecked<4>();
     size_t one_bs_data = input_channels*feat_h*feat_w;
     size_t one_bs_kernel = input_channels*kh*kw;
-    for(int b = 0; i < db; ++b) {
+    for(int b = 0; b < db; ++b) {
         for(int out = 0; out < output_channels; ++out) {
             for(int i = 0; i < dh+padding*2-kh+1; i += stride) {
                 for(int j = 0; j < dw+padding*2-kw+1; j += stride) {
-                    input_data = &(feat_input_ptr[b*one_bs_data+i*feat_w+j])
-                    kernel_data = &(kernel_ptr[out*one_bs_kernel]);
-                    output(b, out, i/stride, j/stride) = multiply(kernel_data, input_data, feat_h, feat_w, kh, kw, input_channels)+bias[out];
+                    float * input_data = &(feat_input_ptr[b*one_bs_data+i*feat_w+j]);
+                    float * kernel_data = &(kernel_ptr[out*one_bs_kernel]);
+                    output(b, out, i/stride, j/stride) = multiply(kernel_data, input_data, feat_h, feat_w, kh, kw, input_channels)+bias.at(out);
                 }
             }
         }
@@ -111,40 +109,29 @@ void conv2d_forward_nobias(const py::array_t<float>& feat_input, const py::array
     conv2d_forward(feat_input, kernel, bias, padding_feat, feat_output, stride, padding);
 }
 
-                bs, input_channels, dh, dw = self.creator[0].shape
-                output_channels, ki, kh, kw = self.creator[1].shape
-                
-                # grad for bias
-                if self.has_bias:
-                    grad_bias = self.grad.data.sum(axis=(0,2,3))
-                    self.creator[2].backward(Tensor(grad_bias), self)
+void calc_grad_kernel(const float * input_data, py::array_t<float>& grad_kernel, float grad_number, \
+                      int out_channel_indx, int input_channles, int kh, int kw, int dh, int dw)
+{
+     auto grad = grad_kernel.mutable_unchecked<4>();
+     for(int i = 0; i < input_channles; ++i) {
+         for(int h = 0; h < kh; ++h){
+             for(int w = 0; w < kw; ++w)
+                 grad(out_channel_indx, i, h, w) = grad_number*input_data[i*dh*dw+h*dw+w];
+         }
+     }
+}
 
-                # grad for kernel
-                input_data = self.creator[0].data
-                padding = self.padding
-                if padding>0:
-                    input_data = self.padding_input_data
-                grad_kernel = np.zeros((output_channels, input_channels, kh, kw))
-                for b in range(bs):
-                    for out in range(output_channels):
-                        for i in range(0, dh+padding*2-kh+1, self.stride):
-                            for j in range(0, dw+padding*2-kw+1, self.stride):
-                                input = input_data[b, :, i:i+kh, j:j+kw]  # input_channels*kh*kw
-                                grad_kernel[out] += input*self.grad.data[b, out, i//self.stride, j//self.stride]
-                if padding>0:
-                    del self.padding_input_data
-                self.creator[1].backward(Tensor(grad_kernel), self)
-
-                # grad for input data
-                kernel = self.creator[1].data
-                grad_input = np.zeros((bs, input_channels, dh+padding*2, dw+padding*2))
-                for b in range(bs):
-                    for out in range(output_channels):
-                        for i in range(0, dh+padding*2-kh+1, self.stride):
-                            for j in range(0, dw+padding*2-kw+1, self.stride):
-                                grad_input[b, :, i:i+kh, j:j+kw] += kernel[out]*self.grad.data[b, out, i//self.stride, j//self.stride]
-                self.creator[0].backward(Tensor(grad_input[:,:,padding:dh+padding,padding:dw+padding]), self)
-
+void calc_grad_input(const float * kernel_data, py::array_t<float>& grad_input, float grad_number, \
+                      int out_channel_indx, int input_channles, int kh, int kw, int dh_start, int dw_start)
+{
+     auto grad = grad_input.mutable_unchecked<4>();
+     for(int i = 0; i < input_channles; ++i) {
+         for(int h = dh_start; h < dh_start+kh; ++h){
+             for(int w = dw_start; w < dw_start+kw; ++w)
+                 grad(out_channel_indx, i, h, w) = grad_number*kernel_data[i*kh*kw+h*kw+w-dw_start];
+         }
+     }
+}   
 
 void conv2d_backward(const py::array_t<float>& grad_output, const py::array_t<int>& feat_input, const py::array_t<int>& kernel, const py::array_t<int>& bias, \
                      py::array_t<float>& input_grad, py::array_t<float>& kernel_grad, py::array_t<float>& bias_grad, int stride, int padding)
@@ -152,7 +139,6 @@ void conv2d_backward(const py::array_t<float>& grad_output, const py::array_t<in
     py::buffer_info grad_output_buf = grad_output.request();
     py::buffer_info feat_input_buf = feat_input.request();
     py::buffer_info kernel_buf = kernel.request();
-    py::buffer_info bias_buf = bias.request();
     
     size_t bs = feat_input_buf.shape[0];
     size_t input_channels = feat_input_buf.shape[1];
@@ -170,58 +156,33 @@ void conv2d_backward(const py::array_t<float>& grad_output, const py::array_t<in
     }
 
   
+    // grad for bias
     size_t gh = grad_output_buf.shape[2];
     size_t gw = grad_output_buf.shape[3];
-    auto output = grad_output.unchecked<4>();
-    for(int b = 0; b < bs; ++b) {
-        for(int i = 0; i < output_channels; ++i) {
+    auto grad_output_arr = grad_output.unchecked<4>();
+    auto bias_grad_arr = bias_grad.mutable_unchecked<1>();
+    for(size_t b = 0; b < bs; ++b) {
+        for(size_t i = 0; i < output_channels; ++i) {
             float grad_sum = 0;
-            for(int j = 0; j < gh; ++j)
-                for(int k = 0; k < gw; ++k)
-                    grad_sum += output(b, i, j, k);
-            grad_bias[i] += grad_sum;
+            for(size_t j = 0; j < gh; ++j)
+                for(size_t k = 0; k < gw; ++k)
+                    grad_sum += grad_output_arr(b, i, j, k);
+            bias_grad_arr(i) += grad_sum;
         }
     }
-    
-    grad_bias = self.grad.data.sum(axis=(0,2,3))
-    self.creator[2].backward(Tensor(grad_bias), self)
 
-    // the buffer info for array
-    py::buffer_info grad_output_buf = grad_output.request();
-    py::buffer_info grad_input_buf = grad_input.request();
-    // the channel & height & width for grad input buf, the same shape with feat_x in forward function
-    if (grad_input_buf.shape[0] != 1)  // input grad's shape is assumed to be 1*c*h*w, a typical value is 1*512*37*50
-    {
-        throw std::runtime_error("we only support batch==1 right now!");
-    }
-
-    auto grad_output_cache = grad_output.unchecked<4>();
-    auto feat_pos_cache = feat_pos.unchecked<4>();
-    auto grad_input_cache = grad_input.mutable_unchecked<4>();
-
-    // the batch size of grad output from upstream
-    size_t bs_grad_output = grad_output_buf.shape[0];
-    // the channel number of grad input
-    size_t c = grad_input_buf.shape[1];
-
-    // the base offset for buffers
-    int num_feat_map = roi_size*roi_size;
-    // iterator the grad one by one 
-    for(int i = 0; i < bs_grad_output; ++i)
-    {
-        for(int ch = 0; ch < c; ++ch) {
-            for(int idx = 0; idx < num_feat_map; ++idx) {
-                int y = idx/roi_size;
-                int x = idx%roi_size;
-            
-                // the feat pos has recorded the position where the max value from during forwarding procedure
-                int max_feat_pos_y = feat_pos_cache(i, ch, idx, 0);
-                int max_feat_pos_x = feat_pos_cache(i, ch, idx, 1);
-                // add grad from upstream
-                grad_input_cache(0, ch, max_feat_pos_y, max_feat_pos_x) += \
-                grad_output_cache(i, ch, y, x);
-            }
-        }
+    // grad for kernel & input data
+    float * input_feat = (float*)feat_input_buf.ptr;
+    float * kernel_feat = (float*)kernel_buf.ptr;
+    for(size_t b = 0; b < bs; ++b) {
+        for(size_t out = 0; out < output_channels; ++out)
+            for(size_t i = 0; i < dh+padding*2-kh+1; i += stride)
+                for(size_t j = 0; j < dw+padding*2-kw+1; j += stride) {
+                    float * input_data = input_feat+b*input_channels*dh*dw+i*dw+j;
+                    float * kernel_data = kernel_feat+out*input_channels*kh*kw;
+                    calc_grad_kernel(input_data, kernel_grad, grad_output_arr(b, out, i/stride, j/stride), out, input_channels, kh, kw, dh, dw);
+                    calc_grad_input(kernel_data, input_grad, grad_output_arr(b, out, i/stride, j/stride), out, input_channels, kh, kw, i, j);
+                }
     }
 }
 
@@ -232,5 +193,5 @@ PYBIND11_MODULE(conv_operations, m) {
     m.def("add", &add, "for test");
     m.def("conv2d_forward_withbias", &conv2d_forward_withbias, "A function do conv2d forward(with bias) operation according to input features & kernel");
     m.def("conv2d_forward_nobias", &conv2d_forward_nobias, "A function do conv2d forward(without bias) operation according to input features & kernel");
-    m.def("roi_pooling_backward", &roi_pooling_backward, "A function do roi pooling backward operation according to grad output");
+    m.def("conv2d_backward", &conv2d_backward, "A function do conv2d backward operation according to grad output");
 }
---
+++ framework/conv_operations/test/test.py
@@ -0,0 +1,9 @@
+import torch as t
+import numpy as np
+import conv_operations as co
+
+
+
+
+
+assert co.add(1, 2) == 3
